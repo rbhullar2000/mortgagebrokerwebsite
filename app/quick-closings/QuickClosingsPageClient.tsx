d44a8590d08d1ffab6ecd7c7ad5f9a68822b24f1
rbhullar2000 <robsbhullar@gmail.com>
2025-06-16 16:22:47 -0700

    Update QuickClosingsPageClient.tsx

--- app/quick-closings/QuickClosingsPageClient.tsx
+++ app/quick-closings/QuickClosingsPageClient.tsx
@@ -244,9 +244,9 @@ export default function QuickClosingsPageClient() {
               </p>
               <div className="flex flex-col sm:flex-row gap-4 justify-center">
                 <Button size="lg" className="bg-orange-600 hover:bg-orange-700" asChild>
-                  <Link href="tel:+16041234567">
+                  <Link href="tel:+17788897964">
                     <Phone className="w-5 h-5 mr-2" />
-                    Emergency Line: (604) 123-4567
+                    Emergency Line: (778) 889-7964
                   </Link>
                 </Button>
                 <Button size="lg" variant="outline" asChild>
