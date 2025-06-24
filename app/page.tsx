2680c9cd45cbfa4a8cec28a36f761f54f64c8b87
rbhullar2000 <robsbhullar@gmail.com>
2025-06-24 01:15:57 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -367,9 +367,13 @@ export default function HomePage() {
 
               
               <div className="flex flex-col sm:flex-row gap-4">
-                <Button size="lg" className="bg-[#032133] hover:bg-[#032133]/90" asChild>
-                  <Link href="mailto:rob@bcmortgageteam.com">Email Rob</Link>
-                </Button>
+                <Button
+  size="lg"
+  className="bg-[#032133] hover:bg-[#032133]/90"
+  onClick={() => window.location.href = 'mailto:rob@bcmortgageteam.ca'}
+>
+  Email Rob
+</Button>
                 <Button
                   size="lg"
                   variant="outline"
