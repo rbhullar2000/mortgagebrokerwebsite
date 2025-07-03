8425afbfc4e9110fe2f82f1be765532e06463186
rbhullar2000 <robsbhullar@gmail.com>
2025-07-03 13:47:51 -0700

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -52,6 +52,16 @@ export function Header() {
             </Link>
           </div>
 
+<!-- Twitter conversion tracking base code -->
+<script>
+!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+twq('config','q3hms');
+</script>
+<!-- End Twitter conversion tracking base code -->
+          
+
           {/* Desktop Navigation */}
           <nav className="hidden lg:flex items-center space-x-6">
             {navigationItems.map((item) => (
