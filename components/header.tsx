142d85b84f489833f13552dd43b0d8f3fce70c53
rbhullar2000 <robsbhullar@gmail.com>
2025-07-13 19:01:43 -0700

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -16,6 +16,7 @@ export function Header() {
     { href: "/purchase", label: "Purchase" },
     { href: "/renewal", label: "Renewal" },
     { href: "/#calculator", label: "Mortgage Calculator" },
+    { href: "/break-even-calculator", label: "Refinance Calculator" },
     { href: "/blog", label: "Blog & Resources" },
     { href: "/#contact", label: "Contact" },
   ]
