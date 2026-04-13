933795707a5da9074f5a4e4afc048871eded480b
rbhullar2000 <robsbhullar@gmail.com>
2026-04-13 12:29:35 -0700

    Replace Twitter widget script with iframe

--- components/twitter-feed.tsx
+++ components/twitter-feed.tsx
@@ -1,44 +1,14 @@
 'use client'
 
-import { useEffect } from 'react'
-
-declare global {
-  interface Window {
-    twttr?: any
-  }
-}
-
 export function TwitterFeed() {
-  useEffect(() => {
-    const loadTwitter = () => {
-      if (window.twttr && window.twttr.widgets) {
-        window.twttr.widgets.load()
-      }
-    }
-
-    const scriptId = 'twitter-wjs'
-
-    if (!document.getElementById(scriptId)) {
-      const script = document.createElement('script')
-      script.id = scriptId
-      script.src = 'https://platform.twitter.com/widgets.js'
-      script.async = true
-      script.onload = loadTwitter
-      document.body.appendChild(script)
-    } else {
-      loadTwitter()
-    }
-  }, [])
-
   return (
-    <div className="w-full min-h-[600px]">
-      <a
-        className="twitter-timeline"
-        href="https://twitter.com/robbhullar"
-        data-height="600"
-      >
-        Tweets by @robbhullar
-      </a>
+    <div className="w-full">
+      <iframe
+        src="https://platform.twitter.com/widgets/timeline/profile?screen_name=robbhullar"
+        width="100%"
+        height="600"
+        style={{ border: 'none' }}
+      />
     </div>
   )
 }
