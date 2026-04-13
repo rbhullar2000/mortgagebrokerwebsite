c45fa8a978bffa750e28f1092da0f2545a115c9c
rbhullar2000 <robsbhullar@gmail.com>
2026-04-13 11:17:05 -0700

    Update twitter-feed.tsx

--- components/twitter-feed.tsx
+++ components/twitter-feed.tsx
@@ -1,22 +1,66 @@
 'use client'
 
-import Script from 'next/script'
+import { useEffect, useRef } from 'react'
+
+declare global {
+  interface Window {
+    twttr?: {
+      widgets?: {
+        load: (element?: HTMLElement | null) => void
+      }
+    }
+  }
+}
 
 export function TwitterFeed() {
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const renderWidget = () => {
+      if (window.twttr?.widgets && containerRef.current) {
+        window.twttr.widgets.load(containerRef.current)
+      }
+    }
+
+    const existingScript = document.querySelector(
+      'script[src="https://platform.twitter.com/widgets.js"]'
+    ) as HTMLScriptElement | null
+
+    if (existingScript) {
+      if (window.twttr?.widgets) {
+        renderWidget()
+      } else {
+        existingScript.addEventListener('load', renderWidget)
+        return () => {
+          existingScript.removeEventListener('load', renderWidget)
+        }
+      }
+      return
+    }
+
+    const script = document.createElement('script')
+    script.src = 'https://platform.twitter.com/widgets.js'
+    script.async = true
+    script.charset = 'utf-8'
+    script.onload = renderWidget
+    document.body.appendChild(script)
+
+    return () => {
+      script.onload = null
+    }
+  }, [])
+
   return (
-    <div className="w-full min-h-[600px]">
+    <div ref={containerRef} className="w-full min-h-[600px]">
       <a
         className="twitter-timeline"
-        data-height="600"
         href="https://twitter.com/robbhullar"
+        data-height="600"
+        data-theme="light"
+        data-chrome="noheader nofooter transparent"
       >
         Tweets by robbhullar
       </a>
-
-      <Script
-        src="https://platform.twitter.com/widgets.js"
-        strategy="lazyOnload"
-      />
     </div>
   )
 }
