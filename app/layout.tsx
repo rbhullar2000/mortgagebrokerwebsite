c3ceea8d6ae90efd04903cf69c60496400459e8b
rbhullar2000 <robsbhullar@gmail.com>
2026-05-06 22:52:50 -0700

    Integrate Google Tag Manager into layout
    Added Google Tag Manager scripts and noscript fallback to the layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,6 +67,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KFT4WFV9');
+          `}
+        </Script>
+
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -104,8 +118,22 @@ export default function RootLayout({
           }}
         />
       </head>
+
       <body className={inter.className}>
-        <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KFT4WFV9"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        <Suspense fallback={<div>Loading...</div>}>
+          {children}
+        </Suspense>
+
         <ChatWidget />
       </body>
     </html>
