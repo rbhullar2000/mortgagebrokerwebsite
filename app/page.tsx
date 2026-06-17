079531920c0a4794bfeccef432aab7458d69ac6b
rbhullar2000 <robsbhullar@gmail.com>
2026-06-17 12:47:44 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -106,7 +106,7 @@ export default function HomePage() {
               </div>
           </div>
         </div>
-      </section>
+      </section> 
 
       {/* Rate Comparison Tool Section */}
       {/*  <section id="rates" className="py-20 bg-slate-50">
