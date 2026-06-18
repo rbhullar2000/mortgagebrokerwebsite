7ffcb0fde1a1b589ee9389f5f0448c9ab88bdd14
rbhullar2000 <robsbhullar@gmail.com>
2026-06-17 17:54:17 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -64,11 +64,7 @@ export default function HomePage() {
                   </Link>
                 </Button>
                
-            <br />
-            <br />   
-        
-
-              
+            <br />     
                 
                 <Button size="lg" variant="outline" className="border-[#1D2D44] text-[#1D2D44] hover:bg-[#1D2D44] hover:text-white h-12 font-semibold bg-transparent" asChild>
                   <Link href="/mortgage-checker">
