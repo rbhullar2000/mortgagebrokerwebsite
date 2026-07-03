0791e3f8e1ae07ec22facb59a03c465f1bce2bf4
rbhullar2000 <robsbhullar@gmail.com>
2026-07-02 23:39:47 -0700

    Change text from 'Independent Advice' to 'Strategy Built Around You'

--- app/page.tsx
+++ app/page.tsx
@@ -115,7 +115,7 @@ export default function HomePage() {
 
           <div className="flex items-center">
             <TrendingUp className="w-5 h-5 mr-2 text-[#C79A2B] flex-shrink-0" />
-                Independent Advice
+                Strategy Built Around You
           </div>
         </div>
       </div>
