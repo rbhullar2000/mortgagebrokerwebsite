7b12be4af3f531dcbaaf22d6e0beff09f9d78dd1
rbhullar2000 <robsbhullar@gmail.com>
2026-07-02 20:55:13 -0700

    Update text to use '&' instead of 'and'

--- app/page.tsx
+++ app/page.tsx
@@ -117,7 +117,7 @@ export default function HomePage() {
 
           <div className="flex items-center">
             <TrendingUp className="w-5 h-5 mr-2 text-[#C79A2B]" />
-            Focus on both Strategy and Rate
+            Focus on both Strategy & Rate
           </div>
         </div>
       </div>
